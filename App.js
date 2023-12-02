07fcfeee9e5ad840b2b27bdf48e8f6576c4394f4
ganyunhee <eunicenellgan@gmail.com>
2023-12-03 03:03:20 +0900

    Edit App.js
     Date:      Sun Dec 3 03:03:20 2023 +0900
     On branch main
     Changes to be committed:
    	modified:   App.js

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>This will be a laundry app!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +19,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: "grey",
+    padding: 10
+  },
+  boldText: {
+    fontWeight: "bold",
+  }
 });
